bbe1568e4ffa6b33c49e0c5cd7e8ba34f573ce46
massakai <masashi.sakai1986@gmail.com>
2013-09-01 01:48:02 +0900

    fix encoding

--- src/intersect.hpp
+++ src/intersect.hpp
@@ -3,6 +3,6 @@
 typedef unsigned DocId;
 typedef std::vector<DocId> PostingList;
 
-// $B#2$D$N%]%9%F%#%s%0%j%9%H$N6&DL=89g$r5a$a$k4X?t(B
+// ２つのポスティングリストの共通集合を求める関数
 void intersect(const PostingList &p1, const PostingList &p2, PostingList &result);
 
